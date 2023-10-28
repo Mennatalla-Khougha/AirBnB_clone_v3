64e134c423827affd23749d8deecd6a11a247357
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-28 13:36:03 +0000

    task 14

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,13 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """get and set password property"""
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        """hashing password"""
+        self.__password = hashlib.md5(value.encode()).hexdigest()
